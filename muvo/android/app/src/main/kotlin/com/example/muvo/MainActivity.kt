7efcce9e7f39f6765a1d36bb58ac8bdb72437e83
AndresCadena <andres.cadena.alanis@gmail.com>
2025-05-25 21:12:52 -0600

    Limpieza de carpetas

--- muvo/android/app/src/main/kotlin/com/example/muvo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.muvo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- muvo/lib/features/movies/domain/repositories/movie_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:muvo/features/movies/domain/entities/movie.dart';
-
-abstract class MovieRepository {
-  Future<List<Movie>> getPopularMovies();
-  Future<List<Movie>> getTopRatedMovies();
-  Future<List<Movie>> getUpcomingMovies();
-  Future<Movie> getMovieDetails(int id);
-  Future<List<Movie>> searchMovies(String query);
-} 
